4a1c18584e348fcc852d8c02fbbd5024decc2867
Olabode Joseph Oluwaseun <josepholabode17@gmail.com>
2022-10-22 03:37:53 +0100

    implemented the factory patterns

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = ShapeFactory.getShape("rectangle");
+            rectangle.draw();
+
+            Shape triangle = ShapeFactory.getShape("triangle");
+            triangle.draw();
+
+            Shape circle = ShapeFactory.getShape("circle");
+            circle.draw();
+        }
+    }
+
+    interface Shape
+    {
+        void draw();
+    }
+
+    class Rectangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Drawing a Rectangle");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Drawing a Triangle");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+    }
+
+    class ShapeFactory
+    {
+        public static Shape getShape(string type)
+        {
+            if (type.ToLower() == "rectangle")
+                return new Rectangle();
+            else if (type.ToLower() == "triangle")
+                return new Triangle();
+            else
+                return new Circle();
+        }
+    }
+}
+
